f89b6131ecdbeba4aed1568fae6f982145228bd2
rishikeshgawade <rishikesh.gawade@niyuj.com>
2022-02-08 17:38:42 +0530

    string operations

---
+++ calculate_average.pl
@@ -0,0 +1,14 @@
+# this program is for calculate average of array elements
+@numbers=(10,20,30,40,50); #int(<STDIN>);
+print(@number);
+$count=@number;
+print($count);
+$sum=0;
+foreach $num (@number)
+{
+	$sum+=$num;
+}
+
+print("Average is =$sum\n");
+
+print("\n @number\n");
